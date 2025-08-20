b67b9f4b8163d234cac3f721fe20dbd259fbb3ce
Mokhanes <mokhanes11@gmail.com>
2025-08-20 10:33:26 +0530

    Add:Added the TestFile.

---
+++ Java/Tree DSA/TesrFile.java
@@ -0,0 +1,9 @@
+public class TesrFile {
+    public static void main(String[] args) {
+        // Example usage of TreeUtils to build and print a binary tree
+        Integer[] arr = {1, 2, 3, null, 4, 5, 6};
+        TreeNode root = TreeUtils.buildBT(arr);
+        System.out.println("Binary Tree:");
+        TreeUtils.printTree(root, 0);
+    }
+}
