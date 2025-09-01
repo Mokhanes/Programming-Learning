443c73b87688d80cd91a7f662126ddfc59d3de9a
Mokhanes <mokhanes11@gmail.com>
2025-09-01 17:38:25 +0530

    Feat:Add the functions

--- Java/DSA Strivers/Basic Maths/BasicMaths.java
+++ Java/DSA Strivers/Basic Maths/BasicMaths.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 
 public class BasicMaths {
+    // Count number of digits in a number
     public static int countDigit(int n){
         int count = 0;
         while(n!=0){
@@ -12,6 +13,7 @@ public class BasicMaths {
         return count;
     }
 
+    // Reverse a number
     public static int reverseNumber(int n){
         int rev = 0;
         while(n!=0){
@@ -22,13 +24,46 @@ public class BasicMaths {
         return rev;
     }
 
+    // Check if a number is palindrome
     public static boolean palindrome(int n){
         return n == reverseNumber(n);
     }
 
+    // Find GCD of two numbers
     public static int findGCD(int a, int b){
         if(b==0) return a;
         return findGCD(b, a%b);
+    }
+
+    // Check if a number is Armstrong number
+    public static boolean amstrong(int n){
+        int original = n;
+        int sum = 0;
+        int digits = countDigit(n);
+        while(n!=0){
+            int lastDigit = n%10;
+            sum += Math.pow(lastDigit, digits);
+            n = n/10;
+        }
+        return sum == original;
+    }
+
+    // Print all divisors of a number
+    public static void divisors(int n){
+        for(int i=1; i<=Math.sqrt(n); i++){
+            if(n%i==0){
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    // Check if a number is prime
+    public static boolean isPrime(int n){
+        if(n<=1) return false;
+        for(int i=2; i<=Math.sqrt(n); i++){ // i*i<=n
+            if(n%i==0) return false;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
