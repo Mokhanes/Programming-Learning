a0bd6d52d9bf8f20642875b1b5d62861f9cada2d
Mokhanes <mokhanes11@gmail.com>
2025-09-15 18:56:57 +0530

    Feat: Add insertionSort method to SortingTechniques class

--- Java/DSA Strivers/Sorting/SortingTechniques.java
+++ Java/DSA Strivers/Sorting/SortingTechniques.java
@@ -51,6 +51,28 @@ class SortingTechniques {
         }
     }
 
+
+    public static void insertionSort(int arr[]){
+        /* Time Complexity:
+        *   Best Case:    O(n)      // When the array is already sorted
+        *   Average Case: O(n^2)
+        *   Worst Case:   O(n^2)
+        * 
+        * Space Complexity: O(1)
+        *   Auxiliary Space: O(1)
+        */
+        int n = arr.length;
+        for(int i =1;i<n;i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j>=0 && arr[j] > key){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = key;
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = {5,4,3,2,1};
         bubbleSort(arr);
