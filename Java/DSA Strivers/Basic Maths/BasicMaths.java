a6cf6170362b0b233c63a916fbf91d1599732ee9
Mokhanes <mokhanes11@gmail.com>
2025-09-17 18:29:03 +0530

    Refactor: Comment out user input and digit count in main method for testing purposes

--- Java/DSA Strivers/Basic Maths/BasicMaths.java
+++ Java/DSA Strivers/Basic Maths/BasicMaths.java
@@ -68,9 +68,10 @@ public class BasicMaths {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number: ");
-        int n = sc.nextInt();
-        System.out.print("Number of digits in " + n + " is: " + countDigit(n));
-        sc.close();
+        // System.out.print("Enter the number: ");
+        // int n = sc.nextInt();
+        // System.out.print("Number of digits in " + n + " is: " + countDigit(n));
+        // sc.close();
+        //System.out.print(findGCD(126,162));
     }
 }
